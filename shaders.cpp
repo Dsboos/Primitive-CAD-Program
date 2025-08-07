82285ae8f875465987b9c0a0dd34c44a5e1816cc
Dsboos <dsbossofuall@gmail.com>
2025-08-08 02:49:33 +0530

    Changed implementation of shape plotting. Made shapes into edittable objects that are stored in a hashmap in the viewport. Any change in shape line_points vector will be dynamically updated in viewport by calling the encodePoints() method now. Also added translate and scaling methods for all current shape objects (lines, circles and bezier curves)

--- shaders.cpp
+++ shaders.cpp
@@ -18,8 +18,7 @@ const int RESOLUTION = RESOLUTION_X * RESOLUTION_Y;
 class Screen
 {
 public:
-    int resolution_x;
-    int resolution_y;
+    int resolution_x, resolution_y;
     Screen(int custom_resolution_x, int custom_resolution_y)
     {
         resolution_x = custom_resolution_x;
@@ -87,17 +86,31 @@ public:
 class CoordinatePlane
 {
 public:
-    int resolution_x;
-    int resolution_y;
-    int resolution;
+    int resolution_x, resolution_y, resolution_z, resolution;
     CoordinatePlane(int input_resolution_x, int input_resolution_y)
     {
         resolution_x = input_resolution_x;
         resolution_y = input_resolution_y;
         resolution = resolution_x * resolution_y;
     }
+    CoordinatePlane(int input_resolution_x, int input_resolution_y, int input_resolution_z)
+    {
+        resolution_x = input_resolution_x;
+        resolution_y = input_resolution_y;
+        resolution_z = input_resolution_z;
+        resolution = resolution_x * resolution_y;
+    }
     // Returns index of array that corresponds to specified coordinates
     int coords(int x, int y)
+    {
+        if (x > resolution_x || 0 > x)
+            return 0;
+        if (y > resolution_y || 0 > y)
+            return 1;
+        int coord_index = (resolution - (y * resolution_x)) - (resolution_x - x);
+        return coord_index;
+    }
+    int coords3D(int x, int y, int z)
     {
         if (x > resolution_x || 0 > x)
             return 0;
@@ -114,8 +127,7 @@ public:
 class Point
 {
 public:
-    int x;
-    int y;
+    int x, y;
     bool isDisplayed = false;
     Point(int x_value, int y_value)
     {
@@ -142,9 +154,9 @@ public:
             line_points[i]->Translate(x_value, y_value);
         }
     }
-    vector<Point *> getLinePoints()
+    vector<Point *> *getLinePoints()
     {
-        return line_points;
+        return &line_points;
     }
 };
 
@@ -153,27 +165,46 @@ public:
 class Line : public geometricObject
 {
 public:
+    pair<double, double> p1, p2;
     double m;
     int c;
     Line(Point *point1, Point *point2)
     {
-        double x1 = point1->x, y1 = point1->y, x2 = point2->x, y2 = point2->y;
-        generateLinePoints(x1, y1, x2, y2);
+        Line({point1->x, point1->y}, {point2->x, point2->y});
     }
-    Line(pair<int, int> p1, pair<int, int> p2)
+    Line(pair<int, int> point1, pair<int, int> point2)
     {
-        generateLinePoints(p1.first, p1.second, p2.first, p2.second);
+        p1 = point1;
+        p2 = point2;
+        generateLinePoints();
     }
-    void generateLinePoints(double x1, double y1, double x2, double y2)
+    void generateLinePoints()
     {
-        double mag = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+        line_points.erase(line_points.begin(), line_points.end());
+        double mag = sqrt(pow((p2.first - p1.first), 2) + pow((p2.second - p1.second), 2));
         for (double t = 0; t < mag; t += 1.414) // increment by sqrt(2).
         {
-            int x = x1 + t * (x2 - x1) / mag;
-            int y = y1 + t * (y2 - y1) / mag;
+            int x = p1.first + t * (p2.first - p1.first) / mag;
+            int y = p1.second + t * (p2.second - p1.second) / mag;
             Point *point = new Point(x, y);
             line_points.push_back(point);
         }
+    }
+    void scale(pair<double, double> scale, pair<int, int> centre)
+    {
+        p1.first = (p1.first - centre.first) * scale.first + centre.first;
+        p1.second = (p1.second - centre.second) * scale.second + centre.second;
+        p2.first = (p2.first - centre.first) * scale.first + centre.first;
+        p2.second = (p2.second - centre.second) * scale.second + centre.second;
+        generateLinePoints();
+    }
+    void translate(pair<int, int> trans)
+    {
+        p1.first += trans.first;
+        p2.first += trans.first;
+        p1.second += trans.second;
+        p2.second += trans.second;
+        generateLinePoints();
     }
 };
 
@@ -182,16 +213,21 @@ public:
 class Circle : public geometricObject
 {
 public:
-    Circle(Point *centre, int radius)
+    int centre_x, centre_y, radius;
+    Circle(Point *centre, int rad)
     {
-        generateLinePoints(centre->x, centre->y, radius);
+        Circle({centre->x, centre->y}, rad);
     }
-    Circle(pair<int, int> centre, int radius)
+    Circle(pair<int, int> centre, int rad)
     {
-        generateLinePoints(centre.first, centre.second, radius);
+        centre_x = centre.first;
+        centre_y = centre.second;
+        radius = rad;
+        generateLinePoints();
     }
-    void generateLinePoints(int centre_x, int centre_y, int radius)
+    void generateLinePoints()
     {
+        line_points.erase(line_points.begin(), line_points.end());
         for (double t = 0; t < (2 * 3.1415); t += 1.0 / radius)
         {
             int x = centre_x + radius * cos(t);
@@ -200,24 +236,47 @@ public:
             line_points.push_back(point);
         }
     }
+    void scale(double scale)
+    {
+        radius *= scale;
+        generateLinePoints();
+    }
+    void translate(pair<int, int> trans)
+    {
+        centre_x += trans.first;
+        centre_y += trans.second;
+        generateLinePoints();
+    }
 };
 
 //--------------------------------------------------------
-//(WIP) Bezier Curve
+// Bezier Curve (Quadratic and Cubic)
 class Bezier : public geometricObject
 {
 public:
-    Bezier(pair<int, int> p1, pair<int, int> p2, pair<int, int> p3)
+    pair<int, int> p1, p2, p3, p4;
+    int degree;
+    Bezier(pair<int, int> ip1, pair<int, int> ip2, pair<int, int> ip3)
     {
-        generateLinePointsQuadratic(p1, p2, p3);
+        p1 = ip1;
+        p2 = ip2;
+        p3 = ip3;
+        degree = 2;
+        generateLinePointsQuadratic();
     }
-    Bezier(pair<int, int> p1, pair<int, int> p2, pair<int, int> p3, pair<int, int> p4)
+    Bezier(pair<int, int> ip1, pair<int, int> ip2, pair<int, int> ip3, pair<int, int> ip4)
     {
-        generateLinePointsCubic(p1, p2, p3, p4);
+        p1 = ip1;
+        p2 = ip2;
+        p3 = ip3;
+        p4 = ip4;
+        degree = 3;
+        generateLinePointsCubic();
     }
 
-    void generateLinePointsQuadratic(pair<int, int> p1, pair<int, int> p2, pair<int, int> p3)
+    void generateLinePointsQuadratic()
     {
+        line_points.erase(line_points.begin(), line_points.end());
         int max_y = max(abs(p2.second - p1.second), abs(p3.second - p1.second));
         int max_x = max(abs(p2.first - p1.first), abs(p3.first - p1.first));
         for (double t = 0; t <= 1; t += 1.0 / max(max_x, max_y))
@@ -228,8 +287,9 @@ public:
             line_points.push_back(point);
         }
     }
-    void generateLinePointsCubic(pair<int, int> p1, pair<int, int> p2, pair<int, int> p3, pair<int, int> p4)
+    void generateLinePointsCubic()
     {
+        line_points.erase(line_points.begin(), line_points.end());
         int max_y = max(max(abs(p2.second - p1.second), abs(p3.second - p1.second)), abs(p4.second - p1.second));
         int max_x = max(max(abs(p2.first - p1.first), abs(p3.first - p1.first)), abs(p4.first - p1.first));
         for (double t = 0; t <= 1; t += 1.0 / max(max_x, max_y))
@@ -240,10 +300,57 @@ public:
             line_points.push_back(point);
         }
     }
+    void scale(pair<double, double> scale, pair<int, int> centre)
+    {
+        p1.first = (p1.first - centre.first) * scale.first + centre.first;
+        p1.second = (p1.second - centre.second) * scale.second + centre.second;
+        p2.first = (p2.first - centre.first) * scale.first + centre.first;
+        p2.second = (p2.second - centre.second) * scale.second + centre.second;
+        p3.first = (p3.first - centre.first) * scale.first + centre.first;
+        p3.second = (p3.second - centre.second) * scale.second + centre.second;
+        if (degree == 3)
+        {
+            p4.first = (p4.first - centre.first) * scale.first + centre.first;
+            p4.second = (p4.second - centre.second) * scale.second + centre.second;
+            generateLinePointsCubic();
+            return;
+        }
+        generateLinePointsQuadratic();
+    }
+    void translate(pair<int, int> trans)
+    {
+        p1.first += trans.first;
+        p2.first += trans.first;
+        p3.first += trans.first;
+        p1.second += trans.second;
+        p2.second += trans.second;
+        p3.second += trans.second;
+        if (degree == 3)
+        {
+            p4.first += trans.first;
+            p4.second += trans.second;
+            generateLinePointsCubic();
+            return;
+        }
+        generateLinePointsQuadratic();
+    }
 };
 
 //------------------------------------------------------------------------------
-// Handles and generates rendering data for graphics
+// 3D Viewport
+// Handles and generates rendering data for 3D graphics
+class Viewport3D
+{
+public:
+    int width;
+    int height;
+    int resolution;
+    double *viewport_data;
+};
+
+//------------------------------------------------------------------------------
+// 2D Viewport
+// Handles and generates rendering data for 2D graphics
 class Viewport
 {
 public:
@@ -253,6 +360,8 @@ public:
     double *viewport_data;
     CoordinatePlane *viewPlane;
     vector<pair<int, int>> displayed_points;
+    vector<geometricObject *> displayed_shapes;
+    unordered_map<geometricObject *, vector<Point *> *> shape_points;
     Viewport(int resolution_x, int resolution_y)
     {
         width = resolution_x;
@@ -285,7 +394,7 @@ public:
 
     //--------------------------------------------------------
     // Plotting object(s) on the viewport.
-    // Coordinate-specified point plotting (DEPRECATED)
+    // Coordinate-specified point plotting
     void plotPoint(int x, int y)
     {
         pair<int, int> point = {x, y};
@@ -300,18 +409,23 @@ public:
         encodePoints();
     }
     // Plots any complex object with a line_points vector
-    void plotObject(vector<Point *> line_points)
+    void plotObject(geometricObject &shape)
     {
-        for (int i = 0; i < line_points.size(); i++)
-        {
-            plotPoint(line_points[i]);
-        }
+        displayed_shapes.push_back(&shape);
+        shape_points[&shape] = shape.getLinePoints();
         encodePoints();
     }
 
     //--------------------------------------------------------
     // Viewport Clearing Methods
     // Clears all points stored in viewport
+    void clearViewportData()
+    {
+        for (int i = 0; i < resolution; i++)
+        {
+            viewport_data[i] = 0;
+        }
+    }
     void clearAllPoints()
     {
         displayed_points.erase(displayed_points.begin(), displayed_points.end());
@@ -322,12 +436,25 @@ public:
     // Encodes all point data from displayed points to viewport data
     void encodePoints()
     {
+        clearViewportData();
         for (auto coord : displayed_points)
         {
             int coord_index = viewPlane->coords(coord.first, coord.second);
             if (0 <= coord_index && coord_index < resolution)
             {
                 viewport_data[coord_index] = 1;
+            }
+        }
+        for (auto shape : shape_points)
+        {
+            vector<Point *> shape_vec = *shape.second;
+            for (Point *pt : shape_vec)
+            {
+                int coord_index = viewPlane->coords(pt->x, pt->y);
+                if (0 <= coord_index && coord_index < resolution)
+                {
+                    viewport_data[coord_index] = 1;
+                }
             }
         }
     }
@@ -342,14 +469,18 @@ int main()
 {
     Screen screen(RESOLUTION_X, RESOLUTION_Y);
     Viewport viewport(RESOLUTION_X, RESOLUTION_Y);
-    Circle circle({RESOLUTION_X / 2, RESOLUTION_Y / 2}, 15);
-    viewport.plotObject(circle.getLinePoints());
+
+    Circle circle({RESOLUTION_X / 2, RESOLUTION_Y / 2}, 50);
+    viewport.plotObject(circle);
 
     Line line({10, 100}, {300, 110});
-    viewport.plotObject(line.getLinePoints());
+    line.translate({100, 100});
+    viewport.plotObject(line);
 
     Bezier curve({470 / 2, RESOLUTION_Y - 250}, {906 / 2, RESOLUTION_Y - 201}, {367 / 2, RESOLUTION_Y - 86}, {650 / 2, RESOLUTION_Y - 31});
-    viewport.plotObject(curve.getLinePoints());
+    viewport.plotObject(curve);
+    curve.translate({50, 50});
+    viewport.encodePoints();
 
     screen.renderToFile(viewport.getViewportData());
 }
